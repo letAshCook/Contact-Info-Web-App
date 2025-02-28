b2295836af5af142cd16747b8621e66678a6cf66
Ashwin-Sundar <129718378+letAshCook@users.noreply.github.com>
2025-02-28 12:05:22 +0800

    add files via upload

---
+++ Contact-Info-web-app/app.js
@@ -0,0 +1,75 @@
+function initialize(){
+    var status = "* offline *";
+    if(navigator.onLine){
+        status="* Online *";
+        retrieveContacts();
+    } else{
+        const localStorage = window.localStorage;
+        if(localStorage){
+            const contacts = localStorage.getItem("contacts");
+            if(contacts){
+                displayContacts(JSON.parse(contacts));
+            }
+        }
+    }
+
+    document.getElementById("status").innerHTML = status;
+
+    document.body.addEventListener(
+        "online",
+        function(){
+            document.getElementById("status").innerHTML = "Online";
+        },
+        false
+    );
+    document.body.addEventListener(
+        "offline",
+        function(){
+            document.getElementById("status").innerHTML = "Offline";
+        },
+        false
+    );    
+        
+}
+
+function retrieveContacts(){
+    const xhr = new XMLHttpRequest();
+    const url = "contacts.json";
+
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4){
+            var contacts = JSON.parse(xhr.response).contacts
+            displayContacts(contacts);
+
+            const localStorage = window.localStorage;
+            if(localStorage){
+                localStorage.setItem("contacts", JSON.stringify(contacts));
+            }
+        }
+    };
+
+    xhr.open("get", url);
+    xhr.send();
+}
+
+function displayContacts(contacts){
+    contacts.forEach(addRow);
+}
+
+function addRow(contacts){
+    var tcontent = document.getElementById("tcontent");
+    var row = tcontent.inertRow();
+
+    var nameCell = row.insertCell();
+    nameCell.setAttribute('data-label', "name");
+    nameCell.innerHTML = contacts.name;
+
+    var addressCell = row.insetCell();
+    addressCell.setAttribute('data-label', "Address");
+    addressCell.innerHTML = contacts.address;
+
+    var mobileCell = row.insertCell();
+    mobileCell.setAttribute('data-label', "Mobile");
+    mobileCell.innerHTML = contacts.mobile;
+}
+
